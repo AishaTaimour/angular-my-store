81c833b77be507bad9d9b22e7d015da4879faca4
AishaTaimour <aishataimour@yahoo.com>
2019-10-19 22:41:58 +0300

    adding  cart service and cart component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,9 @@ import { TopBarComponent } from "./components/top-bar/top-bar.component";
 import { ProductListComponent } from "./components/product-list/product-list.component";
 import { ProductAlertsComponent } from "./components/product-alerts/product-alerts.component";
 import { ProductDetailsComponent } from "./components/product-details/product-details.component";
+import { CartService } from './services/cart.service';
+import { CartComponent } from './components/cart/cart.component';
+
 
 @NgModule({
   imports: [
@@ -16,6 +19,7 @@ import { ProductDetailsComponent } from "./components/product-details/product-de
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
       { path: "products/:productId", component: ProductDetailsComponent },
+      { path: 'cart', component: CartComponent },
     ])
   ],
   declarations: [
@@ -23,9 +27,11 @@ import { ProductDetailsComponent } from "./components/product-details/product-de
     TopBarComponent,
     ProductListComponent,
     ProductAlertsComponent,
-    ProductDetailsComponent
+    ProductDetailsComponent,
+    CartComponent
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  providers: [CartService]
 })
 export class AppModule {}
 
